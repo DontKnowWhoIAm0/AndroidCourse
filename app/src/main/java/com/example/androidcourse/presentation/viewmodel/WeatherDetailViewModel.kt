932cdf5fb2197b65add46edd5ffc89491b9e6321
DontKnowWhoIAm0 <sofyakolodeznikova@gmail.com>
2026-05-18 05:48:47 +0300

    Передача аргумента через di

--- app/src/main/java/com/example/androidcourse/presentation/viewmodel/WeatherDetailViewModel.kt
+++ app/src/main/java/com/example/androidcourse/presentation/viewmodel/WeatherDetailViewModel.kt
@@ -1,35 +1,37 @@
-package com.example.androidcourse.presentation.viewmodel
+    package com.example.androidcourse.presentation.viewmodel
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.androidcourse.domain.model.Weather
-import com.example.androidcourse.domain.usecase.GetWeatherUseCase
-import kotlinx.coroutines.launch
+    import androidx.compose.runtime.mutableStateOf
+    import androidx.lifecycle.ViewModel
+    import androidx.lifecycle.viewModelScope
+    import com.example.androidcourse.di.weather_details.CityName
+    import com.example.androidcourse.domain.model.Weather
+    import com.example.androidcourse.domain.usecase.GetWeatherUseCase
+    import kotlinx.coroutines.launch
+    import androidx.compose.runtime.State
+    import javax.inject.Inject
 
-class WeatherDetailViewModel(
-    val cityName: String,
-    private val getWeatherUseCase: GetWeatherUseCase
-) : ViewModel() {
+    class WeatherDetailViewModel @Inject constructor(
+        @CityName private val cityName: String,
+        private val getWeatherUseCase: GetWeatherUseCase,
+    ) : ViewModel() {
 
-    private val _weather = mutableStateOf<Weather?>(null)
-    val weather: State<Weather?> = _weather
+        private val _weather = mutableStateOf<Weather?>(null)
+        val weather: State<Weather?> = _weather
 
-    private val _error = mutableStateOf<String?>(null)
-    val error: State<String?> = _error
+        private val _error = mutableStateOf<String?>(null)
+        val error: State<String?> = _error
 
-    init {
-        loadDetail()
-    }
+        init {
+            fetchDetail()
+        }
 
-    private fun loadDetail() {
-        viewModelScope.launch {
-            val result = getWeatherUseCase(cityName)
-            result.fold(
-                onSuccess = { _weather.value = it.weather },
-                onFailure = { _error.value = it.message }
-            )
+        private fun fetchDetail() {
+            viewModelScope.launch {
+                val result = getWeatherUseCase(cityName)
+                result.fold(
+                    onSuccess = { _weather.value = it.weather },
+                    onFailure = { _error.value = it.message ?: "Ошибка" }
+                )
+            }
         }
     }
-}
